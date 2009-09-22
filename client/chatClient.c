8eda38355c81b5cf161efa8bef6bd4b794291f9c
Samuel May <sam.m4y@gmail.com>
2009-09-22 13:41:10 +1000

    Add socket connection code. Compiles and appears to run successfully.

--- client/chatClient.c
+++ client/chatClient.c
@@ -15,13 +15,17 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <errno.h>
 #include <error.h>
 #include <unistd.h>
 
 #define PASSWORD "3118miniproject"
 #define USERNAME "sammay"
-#define SERV_TIMEOUT 60
+/* #define SERVER_IP "149.171.92.193" */
+#define SERVER_IP "127.0.0.1"
+#define SERVER_PORT 31180
+#define SERVER_TIMEOUT 60
 
 struct reg_msg {
 	char passwd[16];
@@ -30,11 +34,30 @@ struct reg_msg {
 };
 
 int main (int argc, char **argv) {
-	int sock;
-
-	if ((sock = socket(AF_INET,SOCK_DGRAM,0)) < 0) {
+	int socket_fd;
+	struct sockaddr_in server_addr;
+	/* create socket */
+	if ((socket_fd = socket(AF_INET,SOCK_DGRAM,0)) < 0) {
 		error(EXIT_FAILURE,errno,"socket creation failed");
+	} else {
+		printf("created socket\n");
 	}
-	
+
+	/* zero and assign server address struct  */
+	bzero(&server_addr,sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = SERVER_PORT;
+	if (inet_pton(AF_INET,SERVER_IP,&server_addr.sin_addr) < 0) {
+		error(EXIT_FAILURE,errno,"invalid server IP address");
+	}
+
+	if (connect(socket_fd,
+		    (struct sockaddr *)&server_addr,
+		    sizeof(server_addr)) < 0) {
+		error(EXIT_FAILURE,errno,"could not connect to server");
+	} else {
+		printf("connected to server\n");
+	}
+
 	return 0;
 }
